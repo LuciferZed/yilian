adbdc8f110fc0de185154ec7552b44874f3a72e5
lucifer <851591666@qq,com>
2019-03-13 16:47:50 +0800

    update

--- src/main/java/com/yijie/yilian/controller/TestController.java
+++ src/main/java/com/yijie/yilian/controller/TestController.java
@@ -3,8 +3,6 @@ package com.yijie.yilian.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yijie.yilian.util.Uuid;
-
 @RestController
 @RequestMapping("/test")
 public class TestController {
