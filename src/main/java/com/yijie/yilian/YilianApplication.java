1852154f796e8f41afdd165740ac4acf8a4a3d0b
lucifer <851591666@qq,com>
2019-03-13 09:22:25 +0800

    new a project

---
+++ src/main/java/com/yijie/yilian/YilianApplication.java
@@ -0,0 +1,13 @@
+package com.yijie.yilian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YilianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YilianApplication.class, args);
+	}
+
+}
